1b5d2311caf139187a39f46329d0a99da50244a0
enricotm-fiap <rm94371@fiap.com.br>
2024-03-13 13:57:29 -0300

    add some stuff u know

---
+++ src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
@@ -0,0 +1,20 @@
+package br.com.fiap.checkpoint1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Checkpoint1Controller {
+	
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+	
+	@GetMapping("/ping")
+	public String ping() {
+		return "Pong em "+activeProfile;
+	}
+
+}
